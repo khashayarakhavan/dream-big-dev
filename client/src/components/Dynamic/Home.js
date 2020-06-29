38bf7fcffad23e3928013d83f7d5427465bc84af
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-06-29 04:29:46 -0800

    render Firebase facts from its API using react hooks

---
+++ client/src/components/Dynamic/Home.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import getFacts from "./facts";
+
+// const fact= facts[0];
+
+ function Home(props) {
+   const [count, setCount] = useState(0);
+  //  let date3 = new Date().getTime();
+  //  console.log(date3);
+
+    console.log("From Home :D ", Date.now(), props.facts);
+   
+     const output = props.facts.map((fact, index) => {
+       return <li
+         key={index}>{fact.text}</li>
+     });
+   
+   // console.log(getFacts().text);
+  //  const facts = props.facts.map((fact,i) => {
+  //    return <li key={i}>{fact}</li>
+  //  });
+  //  // const data = props.data;
+  //  const noteList = [1,2,3,4,5];
+  //  const noteList = facts;
+   // console.log(data);
+   
+  //  console.log('from Home :D ', props.facts);
+
+   // Similar to componentDidMount and componentDidUpdate:
+
+  // const data = facts;
+  // console.log(data);
+
+//   const table = data.map((fact, i) => {
+//     return <li key={i}>{fact.text}</li>;
+//   });
+
+  return (<div><p style={{fontSize: 100}}>Hi</p>
+   
+      <p>You clicked {count} times</p>
+      
+      <button onClick={() => setCount(count +1) }>Click me</button>
+    <ul>
+     {output}
+    </ul>
+    
+    </div>
+    );
+};
+
+export default Home;
--- client/src/components/Dynamic/Landing.js
+++ client/src/components/Dynamic/Landing.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+import React, { useState, useEffect } from "react";import { Link } from 'react-router-dom';
 import HeroHeader from '../Static/heroHeader';
 import Booking from '../Static/sectionBooking';
 import SectionStories from '../Static/sectionStories';
@@ -12,23 +12,42 @@ import UpButton from "../Static/UpButton";
 import PopUp from '../Static/popUp';
 
 import '../../res/css/style.css';
+import Home from "./Home";
+import getFacts from "./facts";
+const { log } = console;
 
-const Landing = () => {
+const Landing = () =>  {
+  
+  const [facts, setFacts] = useState([]);
+  const [checker, setChecker] = useState(1);
+
+    useEffect( async () => {
+      await getFacts().then(facts => setFacts(facts));
+      const checker = 0;
+    },checker);
+    //   console.log("From Landing :D : ", R);
+    log('Data fetched successfully and sat the state in Landing.js', facts);
+
+    // console.log('This is A : ', A);
+    
+    // });
     return (
       <div>
         <Navigation key="8" />
         <UpButton />
-        <HeroHeader key='1'/>
+        <HeroHeader key="1" />
         <main>
-          <SectionAbout ID='2' />
-          <SectionFeatures ID='3'/>
-          <SectionTours ID='4' />
-          <SectionStories ID='5'/>
-          <Booking ID='6'/>
-          <SectionFooter ID='7' />
-          <PopUp key='9' />
-        </main> 
-      </div>       
+          
+          <Home facts={facts} />
+          <SectionAbout ID="2" />
+          <SectionFeatures ID="3" />
+          <SectionTours ID="4" />
+          <SectionStories ID="5" />
+          <Booking ID="6" />
+          <SectionFooter ID="7" />
+          <PopUp key="9" />
+        </main>
+      </div>
     );
 };
 
