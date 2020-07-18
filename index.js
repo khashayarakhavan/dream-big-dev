4d44c6bdac6e59b9c74f6c9115ebbbd84cdc1501
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-07-17 21:02:49 -0800

    GCP deployment try-3

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 const app = express();
+const dotenv = require("dotenv");
+const { log } = console;
+
+dotenv.config({ path: './config.env' });
 
 require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
 //Database connect -->
-mongoose.connect(keys.mongoURI, { useUnifiedTopology: true, useNewUrlParser: true }, () => {console.log("connected to ATLAS")});
+mongoose.connect(keys.mongoURI, { useUnifiedTopology: true, useNewUrlParser: true }, () => {console.log("successfully connected to MongoDB ATLAS")});
 // <-- Database connect
 
 // data parsing -->
@@ -47,6 +51,28 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //setup port -->
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const port = process.env.PORT || 5000;
+// app.listen(PORT);
+const server = app.listen(port, () => {
+  log(`Express server started running on port ${port}...`);
+});
 // <-- setup port
+
+// Simple one-liner server run:
+// Const http = require('http);
+// http.createServer(appFunction).listen(3000);
+
+process.on('unhandledRejection', err => {
+  log('UNHANDLED REJECTION! 💥 Shutting down...');
+  log(err.name, err.message);
+  server.close(() => { // close the server right now.
+    process.exit(1); // manually shutdown the application.
+  });
+});
+
+process.on('SIGTERM', () => {
+  log('SIGTERM RECEIVED 👋 Shutting down gracefully...');
+  server.close(() => { // Allows all the pending request to get processed completely before shutdown.
+    log('Process Terminated! 😢');
+  });
+});
